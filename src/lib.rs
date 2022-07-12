ff11bcfe794a77be963f6d321d417052570e83d0
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-07-12 20:52:21 +0200

    [TASK] Removed flag

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ mod option_resolver;
 
 // TODO: Implement flags
 // TODO: Implement argument parser
-// TODO: Implement project meta data
 // TODO: Implement wrapped inputs
 // TODO: Implement file reader
 // TODO: Config reader
